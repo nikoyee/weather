681b452dd53c1317385b42325aa84e9f969dae34
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-10-31 12:24:07 -0700

    need to create a logic to output weather row multiple times

--- App.js
+++ App.js
@@ -3,21 +3,33 @@ import WeatherRow from './components/WeatherRow.js';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  // getWeatherData(){
+  //   fetch('')
+  //     .then((response) => response.json())
+  //     .then((responseJson) => {
+  //       return responseJson.list;
+  //     })
+  //     .catch((error) => { console.error(error) }
+  //   )
+  // }
+
+  // organizeDataByDay(weatherObj){
+  //   let d = {};
+  //   weatherObj.forEach(function(e) {
+  //     let a = {};
+  //     let date = new Date(e.dt_txt).getDate();
+  //      d[date] = a;
+  //   });
+  // }
+
   render() {
-    let days = {
-      m: 'Mon',
-      t: 'Tue',
-      w: 'Wed',
-      th: 'Thu',
-      f: 'Fri'
-    }
     return (
       <View style={styles.container}>
-        <WeatherRow day={days.m}></WeatherRow>
-        <WeatherRow day={days.t}></WeatherRow>
-        <WeatherRow day={days.w}></WeatherRow>
-        <WeatherRow day={days.th}></WeatherRow>
-        <WeatherRow day={days.f}></WeatherRow>
+        <WeatherRow></WeatherRow>
       </View>
     );
   }
