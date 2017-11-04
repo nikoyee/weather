df66193bfb1a6ccfe439e7d8b2c83f629e3feca1
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-11-03 21:43:40 -0700

    update app.js with test data variable

--- App.js
+++ App.js
@@ -2,29 +2,12 @@ import React from 'react';
 import WeatherRow from './components/WeatherRow.js';
 import { StyleSheet, Text, View } from 'react-native';
 
+const data = require('./assets/weather.json');
+
 export default class App extends React.Component {
   constructor(props){
     super(props);
   }
-
-  // getWeatherData(){
-  //   fetch('')
-  //     .then((response) => response.json())
-  //     .then((responseJson) => {
-  //       return responseJson.list;
-  //     })
-  //     .catch((error) => { console.error(error) }
-  //   )
-  // }
-
-  // organizeDataByDay(weatherObj){
-  //   let d = {};
-  //   weatherObj.forEach(function(e) {
-  //     let a = {};
-  //     let date = new Date(e.dt_txt).getDate();
-  //      d[date] = a;
-  //   });
-  // }
 
   render() {
     return (
