cc39b9f7f19e61000a08d7ed7ae9c1ac1ebab931
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-11-05 21:06:44 -0800

    tons of update on app.js
    -the payload and the dateobject had issues. the format that the date
    was being given was not valid to create a date object from so i needed
    to modify it on line 31.
    
    - the way i was comparing in the switch statement was wrong, rookie
    mistake
    
    - the object i was populating did not have an interface similar to the
    push method in an array so i switched to using an array.

--- App.js
+++ App.js
@@ -6,12 +6,12 @@ import WeatherRow from './containers/WeatherRow';
 
 /**
  * Global Variables
- */ 
+ */
 
 const data = require('./assets/weather.json');
-const today = new Date().toDateString();
+const today = new Date();
 
-let weatherObj = { 0: {}, 1: {}, 2: {}, 3: {}, 4: {} };
+let weatherObj = [[0],[1],[2],[3],[4]];
 
 /**
  * Date object does not have a native way to add days x_x
@@ -21,40 +21,48 @@ let weatherObj = { 0: {}, 1: {}, 2: {}, 3: {}, 4: {} };
 Date.prototype.addDays = function(days) {
   var dat = new Date(this.valueOf());
   dat.setDate(dat.getDate() + days);
-  return dat.toDateString();
+  return dat;
 }
 
 export default class App extends React.Component {
 
-  _createWeatherObject(){
+  _populateWeatherObject(){
     for(let d of data.list){
-
-      let day = new Date(d.dt_txt).toDateString();
+      let dd = (d.dt_txt).replace(/\s/g, "T").concat("Z");
+      let day = new Date(dd);
 
       /**
        * There are 5 days worth of data entries and
        * we need to modify the data for our app to use
        */
 
-      switch (day) {
-        case (day == today):
+      switch (day.toDateString()) {
+        case (today.toDateString()):
+          weatherObj[0].push(d);
           break;
-        case (day == today.addDays(1)):
+        case (today.addDays(1).toDateString()):
+          weatherObj[1].push(d);
           break;
-        case (day == today.addDays(2)):
+        case (today.addDays(2).toDateString()):
+          weatherObj[2].push(d);
           break;
-        case (day == today.addDays(3)):
+        case (today.addDays(3).toDateString()):
+          weatherObj[3].push(d);
           break;
-        case (day == today.addDays(4)):
+        case (today.addDays(4).toDateString()):
+          weatherObj[4].push(d);
           break;
         default:
+          console.log(day);
           break;
       }
 
     }
+    console.log(weatherObj[0].length);
   }
 
   render() {
+    this._populateWeatherObject();
     return (
       <View style={styles.container}>
         <WeatherRow data={weatherObj[0]}></WeatherRow>
