3dc09fc5b288d41a82671908acb06dcd2e7fa0f8
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-11-09 21:17:19 -0800

    making the app very simple

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
 import React from 'react'
-import { View } from 'react-native'
+import {
+  View,
+  Text,
+  ScrollView
+} from 'react-native'
+
+import PaddingTop from './App/Containers/PaddingTop'
+
 
 export default class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
   }
+
+  outputCubes = (obj) => {
+    return obj.map(function (d) {
+      return (
+        <View key={d.dt} style={{ width: '33.3%', height: 100, flexWrap: 'wrap' }}>
+          <Text>{d.main.temp}</Text>
+          <Text>{d.weather[0].description}</Text>
+        </View>
+      )
+    })
+  }
+
   render() {
+    const data = require('./App/Assets/weather.json')
     return (
-      <View></View>
+      <ScrollView>
+        <PaddingTop/>
+        <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+          {this.outputCubes(data.list)}
+        </View>
+      </ScrollView>
     )
   }
 }
--- App/Containers/WeatherCube/styles.js
+++
@@ -1,5 +0,0 @@
-import {StyleSheet} from 'react-native'
-
-export default Styles = StyleSheet.create({
-
-})
