152c35a24f18c9769bb8f6f8a583d5c24b675ea0
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-11-10 19:28:55 -0800

    done with the first iteration of the app

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {
   View,
   Text,
+  Image,
   ScrollView,
   StyleSheet
 } from 'react-native'
@@ -15,24 +16,74 @@ export default class App extends React.Component {
   }
 
   outputCubes = (obj) => {
+
+    test = (num) => {
+      switch(num){
+        case(1):
+          return 'Mon'
+          break;
+        case(2):
+          return 'Tue'
+          break;
+        case(3):
+          return 'Wed'
+          break;
+        case(4):
+          return 'Thu'
+          break;
+        case(5):
+          return 'Fri'
+          break;
+        case(6):
+          return 'Sat'
+          break;
+        case(7):
+          return 'Sun'
+          break;
+        default:
+          return '?'
+          break;
+      }
+    }
+
     return obj.map(function (d) {
       const key  = d.dt
       const temp = Math.floor(d.main.temp)
-      const desc = d.weather[0].description
+      const desc = d.weather[0].main
+      const date = test(new Date(d.dt_txt).getDay())
       switch(desc){
-        case('clear sky'):
+        case('Clear'):
           return (
             <View key={key} style={Styles.cubeYellow}>
-              <Text>{temp}</Text>
-              <Text>{desc}</Text>
+              
+              <View style={Styles.centerIcon}>
+                <Text>{date} at 2PM</Text>
+                <Image style={Styles.icon} resizeMode='contain' source={require('./App/Assets/Icons/clear-sky.png')}/>
+                <Text>{temp}<Image style={Styles.tempIcon} source={require('./App/Assets/Icons/fahrenheit.png')}/>  </Text>
+              </View>
+              
             </View>
           )
           break;
-        case('few clouds'):
+        case('Clouds'):
           return (
             <View key={key} style={Styles.cubeBlue}>
-              <Text>{temp}</Text>
-              <Text>{desc}</Text>
+              <View style={Styles.centerIcon}>
+                <Text>{date}</Text>
+                <Image style={Styles.icon} resizeMode='contain' source={require('./App/Assets/Icons/broken-clouds.png')}/>
+                <Text>{temp}<Image style={Styles.tempIcon} source={require('./App/Assets/Icons/fahrenheit.png')}/>  </Text>
+              </View>
+            </View>
+          )
+          break;
+        case('Rain'):
+          return (
+            <View key={key} style={Styles.codePurple}>
+              <View style={Styles.centerIcon}>
+                <Text>{date}</Text>
+                <Image style={Styles.icon} resizeMode='contain' source={require('./App/Assets/Icons/rain.png')}/>
+                <Text>{temp}<Image style={Styles.tempIcon} source={require('./App/Assets/Icons/fahrenheit.png')}/>  </Text>
+              </View>
             </View>
           )
           break;
@@ -40,7 +91,6 @@ export default class App extends React.Component {
           return (
             <View key={key} style={Styles.cubeNone}>
               <Text>{temp}</Text>
-              <Text>{desc}</Text>
             </View>
           )
           break;
@@ -64,14 +114,29 @@ export default class App extends React.Component {
 const cubeContainer = { 
   height: 100,
   width: '33.3%', 
-  flexWrap: 'wrap'  
+  flexWrap: 'wrap',
+  justifyContent: 'center'
 }
 
 const Styles = StyleSheet.create({
   mainContainer: { 
     flexDirection: 'row', 
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
    },
+  centerIcon: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  tempIcon:{
+    height: 12,
+    width: 12
+  },
+  icon: {
+    height: 40,
+    width: 40,
+    marginBottom: 7,
+    marginTop: 7
+  },
   cubeBlue: {
     ...cubeContainer,
     backgroundColor: '#9ebbc6'
@@ -79,6 +144,10 @@ const Styles = StyleSheet.create({
   cubeYellow: {
     ...cubeContainer,
     backgroundColor: '#fdfd96'
+  },
+  codePurple: {
+    ...cubeContainer,
+    backgroundColor: '#DAC2DD'
   },
   cubeNone: {
     ...cubeContainer
