5bc38a0f5b4403c9b2fd357d6d46a48d4abadb51
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-11-12 20:45:13 -0800

    created an external stylesheet for the component

---
+++ App/Containers/WeatherBox/styles.js
@@ -0,0 +1,40 @@
+import {StyleSheet} from 'react-native'
+
+const boxContainer = { 
+  height: 100,
+  width: '33.3%', 
+  flexWrap: 'wrap',
+  justifyContent: 'center'
+}
+
+export default Styles = StyleSheet.create({
+  centerItems: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  tempIcon:{
+    height: 12,
+    width: 12
+  },
+  icon: {
+    height: 40,
+    width: 40,
+    marginBottom: 7,
+    marginTop: 7
+  },
+  blueBox: {
+    ...boxContainer,
+    backgroundColor: '#9ebbc6'
+  },
+  yellowBox: {
+    ...boxContainer,
+    backgroundColor: '#fdfd96'
+  },
+  purpleBox: {
+    ...boxContainer,
+    backgroundColor: '#DAC2DD'
+  },
+  box: {
+    ...boxContainer
+  }
+})
