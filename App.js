ced80974f5a529c3eb6261f0c2ab99e72717dc5c
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-11-18 10:29:27 -0800

    reset app.js

--- App.js
+++ App.js
@@ -8,80 +8,27 @@ import {
 } from 'react-native'
 
 import PaddingTop from './App/Containers/PaddingTop'
-import WeatherBox from './App/Containers/WeatherBox'
-
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
   }
 
-  getWeatherBox = (obj) => {
-    return (
-      <WeatherBox
-        key={obj.key}
-        date={obj.date}
-        temp={obj.temp}
-        tempIcon={obj.tempIconPath}
-        weatherIcon={obj.weatherIconPath}
-      />
-    )
-  }
-
-  outputCubes = (obj) => {
-    const dayArr = [
-      [null],
-      [1, 'Mon'],
-      [2, 'Tue'],
-      [3, 'Wed'],
-      [4, 'Thu'],
-      [5, 'Fri'],
-      [6, 'Sat'],
-      [7, 'Sun']
-    ]
-    return obj.map(function (d) {
-      const weatherBoxData = {
-        key: d.dt,
-        temp: Math.floor(d.main.temp),
-        date: dayArr[(new Date(d.dt_txt).getDay())][0],
-        tempIconPath: './App/Assets/Icons/fahrenheit.png',
-        weatherIconPath: ''
-      }
-      const desc = d.weather[0].main
-      switch(desc){
-        case('Clear'):
-          getWeatherBox(weatherBoxData)
-          break;
-        case('Clouds'):
-          getWeatherBox(weatherBoxData)
-          break;
-        case('Rain'):
-          getWeatherBox(weatherBoxData)
-          break;
-        default:
-          getWeatherBox(weatherBoxData)
-          break;
-      }
-    })
-  }
-
   render() {
-    const data = require('./App/Assets/weather.json')
     return (
       <ScrollView>
         <PaddingTop/>
         <View style={Styles.mainContainer}>
-          {this.outputCubes(data.list)}
+
         </View>
       </ScrollView>
     )
   }
 }
 
-
 const Styles = StyleSheet.create({
-  mainContainer: { 
-    flexDirection: 'row', 
+  mainContainer: {
+    flexDirection: 'row',
     flexWrap: 'wrap',
    }
 })
