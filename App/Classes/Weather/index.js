8c77477a6195411f894a59834feef54ef97eee25
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-11-18 10:51:24 -0800

    imported Icons are rendered as numbers ? WHY ?

--- App/Classes/Weather/index.js
+++ App/Classes/Weather/index.js
@@ -1,11 +1,7 @@
-import clearSkyIcon from '../../Assets/Icons/clear-sky.png'
-import cloudyIcon from '../../Assets/Icons/few-clouds.png'
-import rainIcon from '../../Assets/Icons/rain.png'
-
-export default class Weather {
+class Weather {
   constructor(){
     this.data = require('../../Assets/weather.json')
-    this.weatherList = data.list
+    this.weatherList = this.data.list
     this.dayArr = [
       [null],
       [1, 'Mon'],
@@ -16,23 +12,6 @@ export default class Weather {
       [6, 'Sat'],
       [7, 'Sun']
     ]
-  }
-
-  getTempIcon = (description) => {
-    switch(description){
-      case('Clear'):
-        return clearSkyIcon
-        break
-      case('Clouds'):
-        return cloudyIcon
-        break
-      case('Rain'):
-        return rainIcon
-        break
-      default:
-        return null
-        break
-    }
   }
 
   /**
@@ -46,19 +25,23 @@ export default class Weather {
    *  [7, 'Sun']
    */
   getDayWeather = (dayPicked) => {
-    weatherList.map(
+    let dataForWeatherRowComponent = []
+    this.weatherList.map(
       (weather) => {
         let numberForDayArr = new Date(weather.dt_txt).getDay()
-        let day = dayArr[numberForDayArr][0]
+        let day = this.dayArr[numberForDayArr][1]
         if(day == dayPicked){
-          return {
+          dataForWeatherRowComponent.push({
             key: weather.dt_txt,
             temp: Math.floor(weather.temp),
             time: new Date(weather.dt_txt).getHours(),
-            tempIcon: getTempIcon(weather.weather[0].main)
-          }
+            tempDescription: weather.weather[0].main
+          })
         }
       }
     )
+    return dataForWeatherRowComponent
   }
 }
+
+export default new Weather()
