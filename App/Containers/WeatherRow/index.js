d6b9d056e9100773009702871e1c35cfe0d7e48b
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-11-18 11:36:16 -0800

    implement WeatherRow

--- App/Containers/WeatherRow/index.js
+++ App/Containers/WeatherRow/index.js
@@ -1,20 +1,35 @@
 import React, {Component} from 'react'
 import {
-  View
+  View,
+  Text
 } from 'react-native'
 
 import Weather from '../../Classes/Weather'
 import WeatherBox from '../WeatherBox'
 
 export default class WeatherRow extends Component {
+  _getDayWeather = (dayPicked) => {
+    return Weather.getDayWeather(dayPicked).map(
+      (weather) => {
+        return <WeatherBox
+          key = {weather.key}
+          temp = {weather.temp}
+          time = {weather.time}
+          tempDescription = {weather.tempDescription}
+        />
+      }
+    )
+  }
   render(){
     return(
       <View>
         <View>
           {/* Contains the day string */}
+          <Text></Text>
         </View>
-        <View>
+        <View style={{flexDirection: 'row'}}>
           {/* Contains the weather box for the day */}
+          {this._getDayWeather(this.props.day)}
         </View>
       </View>
     )
