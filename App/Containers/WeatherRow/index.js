34ebbd696cd8cd1a8dca26ba488d08f2513096f3
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-11-28 19:06:48 -0800

    used componentDidMount() to house the fetch logic

--- App/Containers/WeatherRow/index.js
+++ App/Containers/WeatherRow/index.js
@@ -16,42 +16,49 @@ export default class WeatherRow extends Component {
       data: null
     }
   }
-  _getDayWeather = (dayPicked) => {
-    // console.log(Weather.getDayWeather(dayPicked))
-    Weather.getDayWeather(dayPicked).then(results => {
+  _getDayWeather = () => {
+
+    if(this.state.isDataSet){
+      return(
+        <View style={Styles.rowContainer}>
+          <View style={Styles.dayContainer}>
+            <Text style={Styles.dayFont}>{this.props.day[0]}</Text>
+            <Text style={Styles.dayFont}>{this.props.day[1]}</Text>
+            <Text style={Styles.dayFont}>{this.props.day[2]}</Text>
+          </View>
+          <View style={Styles.scrollViewContainer}>
+            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} bonces={false}>
+              {this.state.data.map(
+                (weather) => {
+                  return <WeatherBox
+                    key = {weather.key}
+                    temp = {weather.temp}
+                    time = {weather.time}
+                    tempDescription = {weather.tempDescription}
+                  />
+                }
+              )}
+            </ScrollView>
+        </View>
+      </View>
+      )
+    }
+  }
+
+  componentDidMount(){
+    Weather.getDayWeather(this.props.day)
+    .then( results => {
+      console.log('component did mount')
       this.setState({
         isDataSet: true,
         data: results
       })
     })
-    if(this.state.isDataSet){
-      return this.state.data.map(
-        (weather) => {
-          return <WeatherBox
-            key = {weather.key}
-            temp = {weather.temp}
-            time = {weather.time}
-            tempDescription = {weather.tempDescription}
-          />
-        }
-      )
-    }
   }
-
   render(){
-    if (Weather.getDayWeather(this.props.day).length == 0) {return null}
     return(
-      <View style={Styles.rowContainer}>
-        <View style={Styles.dayContainer}>
-          <Text style={Styles.dayFont}>{this.props.day[0]}</Text>
-          <Text style={Styles.dayFont}>{this.props.day[1]}</Text>
-          <Text style={Styles.dayFont}>{this.props.day[2]}</Text>
-        </View>
-        <View style={Styles.scrollViewContainer}>
-          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} bonces={false}>
-            {this._getDayWeather(this.props.day)}
-          </ScrollView>
-        </View>
+      <View>
+        {this._getDayWeather()}
       </View>
     )
   }
