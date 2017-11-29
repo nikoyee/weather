916b749060f4aafc7f431625e89b9e92add383e0
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-11-28 20:35:41 -0800

    output the next 5 days including today

--- App/Containers/WeatherContainer/index.js
+++ App/Containers/WeatherContainer/index.js
@@ -9,9 +9,34 @@ export default class WeatherContainer extends React.Component {
     super(props)
   }
 
+  weatherStartingToday = () => {
+    const today = new Date().getDay()
+    const dayArr = [
+      [null],
+      [1, 'Mon'],
+      [2, 'Tue'],
+      [3, 'Wed'],
+      [4, 'Thu'],
+      [5, 'Fri'],
+      [6, 'Sat'],
+      [7, 'Sun']
+    ]
+    return(
+      <View>
+        <WeatherRow day={dayArr[today][1]} />
+        <WeatherRow day={dayArr[today+1][1]} />
+        <WeatherRow day={dayArr[today+2][1]} />
+        <WeatherRow day={dayArr[today+3][1]} />
+        <WeatherRow day={dayArr[today+4][1]} />
+      </View>
+    )
+  }
+
   render(){
     return(
-      <View></View>
+      <View>
+        {this.weatherStartingToday()}
+      </View>
     )
   }
 }
