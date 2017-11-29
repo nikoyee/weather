9f0ebf93ba53f17d194965e0b5c4a3a38bef0f8a
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-11-28 19:06:30 -0800

    created a weather container component

---
+++ App/Containers/WeatherContainer/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+  View
+} from 'react-native'
+import WeatherRow from '../WeatherRow'
+
+export default class WeatherContainer extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <View></View>
+    )
+  }
+}
